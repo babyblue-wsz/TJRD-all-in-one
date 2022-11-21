51c449306c33a9e7bf11345aabbf962e7fbc42ec
babyblue <wszwsz179@163.com>
2022-11-21 21:16:24 +0800

    Adjust the segmentatin UI

--- UI/Segmentation.py
+++ UI/Segmentation.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QPushButton, QGridLayout, QFileDialog
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QPushButton, QGridLayout, QFileDialog, QGroupBox
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 
 
 class Segmentation(QWidget):
@@ -9,31 +10,52 @@ class Segmentation(QWidget):
         self.init_layers()
 
     def init(self):
-        self.setFixedWidth(400)
+        self.setFixedWidth(800)
         self.setFixedHeight(300)
 
     def init_layers(self):
+        # 设置整体为一个竖直层
         main_layout = QVBoxLayout()
         self.setLayout(main_layout)
 
+        # 设置上中下三个网格层
         top_layout = QGridLayout()
         main_layout.addLayout(top_layout)
         middle_layout = QGridLayout()
         main_layout.addLayout(middle_layout)
+        bottom_layout = QGridLayout()
+        main_layout.addLayout(bottom_layout)
 
-        title_label = QLabel('这是语义分割')
-        top_layout.addWidget(title_label, 0, 0)
+        # top层用来放两个按钮
+        top_layout.setAlignment(Qt.AlignLeft)
         select_image_btn = QPushButton('选择单张图片')
-        top_layout.addWidget(select_image_btn, 1, 0)
+        top_layout.addWidget(select_image_btn, 0, 0)
+        select_image_btn.setFixedWidth(100)
         select_image_btn.clicked.connect(self.open_single_file)
+
         select_images_btn = QPushButton('选择文件夹')
-        top_layout.addWidget(select_images_btn, 1, 1)
+        top_layout.addWidget(select_images_btn, 0, 1)
+        select_images_btn.setFixedWidth(100)
         select_images_btn.clicked.connect(self.open_folder)
 
+        # middle层用来放两个QGroupBox，分别放输入图片和输出图片的显示
+        input_groupbox = QGroupBox()
+        input_groupbox.setTitle('原始图像输入')
+        middle_layout.addWidget(input_groupbox, 0, 0)
+        input_layout = QVBoxLayout()
         self.input_image = QLabel()
-        middle_layout.addWidget(self.input_image, 0, 0)
+        input_layout.addWidget(self.input_image)
+        self.input_image.setFixedWidth(400)
+        input_groupbox.setLayout(input_layout)
+
+        output_groupbox = QGroupBox()
+        output_groupbox.setTitle('图像输出')
+        middle_layout.addWidget(output_groupbox, 0, 1)
+        output_layout = QVBoxLayout()
         self.output_image = QLabel()
-        middle_layout.addWidget(self.output_image, 0, 1)
+        output_layout.addWidget(self.output_image)
+        self.output_image.setFixedWidth(400)
+        output_groupbox.setLayout(output_layout)
 
     def open_single_file(self):
         file_path = QFileDialog.getOpenFileName(
@@ -43,7 +65,6 @@ class Segmentation(QWidget):
             "图片类型 (*.png *.jpg *.bmp)"  # 选择类型过滤项，过滤内容在括号中
         )
         file_path = file_path[0]
-        print(file_path)
         self.input_image.setPixmap(QPixmap(file_path))
         # 图片大小与label适应，否则图片可能显示不全
         self.input_image.setScaledContents(True)
@@ -53,4 +74,3 @@ class Segmentation(QWidget):
             self,  # 父窗口对象
             "选择你要上传的图片",  # 标题
         )
-        print(file_paths)
--- main.py
+++ main.py
@@ -13,27 +13,32 @@ class MyWidget(QMainWindow):
 
     def init(self):
         self.setWindowTitle('TJRD all in one')
-        self.setFixedSize(800, 600)
-        self.setWindowIcon(QIcon("/Static/tongji.jpg"))
+        self.setFixedSize(820, 600)
+        self.setWindowIcon(QIcon("/Static/images/tongji.jpg"))
 
-        origin_main_window = OriginMainWindow()
-        self.setCentralWidget(origin_main_window)
+        # todo 开发时暂时用语义分割作为主界面
+        self.origin_main_window = OriginMainWindow()
+        # self.setCentralWidget(self.origin_main_window)
+        self.segmentationUI = Segmentation()
+        self.setCentralWidget(self.segmentationUI)
+
 
     def set_menu_bar(self):
         # 创建顶部菜单栏。addMenu()函数创建一级，addAction()函数创建子菜单。
         self.origin = self.menuBar().addMenu('初始')
-        self.settings = QAction('设置', self)
         self.count = QAction('账户', self)
-        self.origin.addAction(self.settings)
+        self.settings = QAction('设置', self)
         self.origin.addAction(self.count)
+        self.origin.addAction(self.settings)
+        self.count.triggered.connect(lambda: self.show_child_UI(self.origin_main_window))
 
         self.functions = self.menuBar().addMenu('工具箱')
         self.segmentation = QAction('语义分割', self)
         self.recognition = QAction('图像识别', self)
         self.functions.addAction(self.segmentation)
         self.functions.addAction(self.recognition)
-        segmentationUI = Segmentation()
-        self.segmentation.triggered.connect(lambda: self.show_child_UI(segmentationUI))
+        # todo 当切到“设置”UI再切到“语义分割”UI时报错
+        self.segmentation.triggered.connect(lambda: self.show_child_UI(self.segmentationUI))
 
     def show_child_UI(self, childUI):
         self.setCentralWidget(childUI)
